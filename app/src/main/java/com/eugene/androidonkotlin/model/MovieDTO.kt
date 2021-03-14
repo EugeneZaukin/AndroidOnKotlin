52dd481ae3d006eccbb06172e90fdf7052dfeba8
Eugene <Eugene_Zaukin@mail.ru>
2021-03-14 13:13:21 +0700

    Отображение фильма DescriptionFragment через API

---
+++ app/src/main/java/com/eugene/androidonkotlin/model/MovieDTO.kt
@@ -0,0 +1,8 @@
+package com.eugene.androidonkotlin.model
+
+data class MovieDTO (
+    val title: String?,
+    val poster: String?,
+    val rating_kinopoisk: String?,
+    val description: String?
+    )
