f69e0a6c4e3ed4a2a180bc7a99352452b3bd0153
Eugene Zaukin <Eugene_Zaukin@mail.ru>
2022-02-02 14:12:01 +0300

    Fix display of the list of movies

--- app/src/main/java/com/eugene/androidonkotlin/view/main/MovieItem.kt
+++ app/src/main/java/com/eugene/androidonkotlin/view/main/MovieItem.kt
@@ -3,22 +3,25 @@ package com.eugene.androidonkotlin.view.main
 import android.view.*
 import com.bumptech.glide.Glide
 import com.eugene.androidonkotlin.R
-import com.eugene.androidonkotlin.databinding.RecyclerItemBinding
+import com.eugene.androidonkotlin.databinding.MainMovieItemBinding
 import com.eugene.androidonkotlin.model.Movie
 import com.mikepenz.fastadapter.binding.AbstractBindingItem
 
-class MovieItem(private val movie: Movie): AbstractBindingItem<RecyclerItemBinding>() {
+class MovieItem(private val movie: Movie): AbstractBindingItem<MainMovieItemBinding>() {
     override val type: Int
         get() = R.id.item_card
 
-    override fun createBinding(inflater: LayoutInflater, parent: ViewGroup?): RecyclerItemBinding {
-        return RecyclerItemBinding.inflate(inflater, parent, false)
+    override fun createBinding(inflater: LayoutInflater, parent: ViewGroup?): MainMovieItemBinding {
+        return MainMovieItemBinding.inflate(inflater, parent, false)
     }
 
-    override fun bindView(binding: RecyclerItemBinding, payloads: List<Any>) {
+    override fun bindView(binding: MainMovieItemBinding, payloads: List<Any>) {
         binding.apply {
             itemTitle.text = movie.title
             itemRating.text = movie.voteAverage.toString()
+
+            //for rounding corners
+            itemImage.clipToOutline = true
 
             Glide.with(root)
                 .load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
